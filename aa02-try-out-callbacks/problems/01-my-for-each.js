deb9fae43a37cac251dd42f84268a459625730b0
Your Name <Your Email>
2024-05-13 18:31:47 -0400

    pr 1, 2 finished

--- aa02-try-out-callbacks/problems/01-my-for-each.js
+++ aa02-try-out-callbacks/problems/01-my-for-each.js
@@ -22,7 +22,11 @@ console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
 
 function myForEach(array, cb) {
-    // Your code here 
+    // Your code here
+    for(let i = 0; i<array.length; i++){
+        let el = array[i];
+        cb(el, i, array);
+    }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
