597912bf8c3bc6e37ed967120cd16aca92bb8433
Your Name <Your Email>
2024-05-13 19:42:10 -0400

    pr 7, 8 finished

--- aa02-try-out-callbacks/problems/07-my-some.js
+++ aa02-try-out-callbacks/problems/07-my-some.js
@@ -24,7 +24,16 @@ console.log(result3);   // true
 *******************************************************************************/
 
 function mySome(array, cb) {
-    // Your code here 
+    // Your code here
+    for(let i=0; i<array.length; i++){
+        let el = array[i];
+        if(cb(el, i)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
