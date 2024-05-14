4a34cda1f231988277dae0776aa47a478005ee16
Your Name <Your Email>
2024-05-13 20:13:58 -0400

    pr 10,11,12 finished

--- aa02-try-out-callbacks/problems/12-exactly.js
+++ aa02-try-out-callbacks/problems/12-exactly.js
@@ -28,7 +28,19 @@ console.log(result4); // true
 *******************************************************************************/
 
 function exactly(array, num, cb) {
-  // Your code here 
+  // Your code here
+  let count =0;
+  for(let i=0; i<array.length; i++){
+    let el=array[i];
+    if(cb(el)){
+      count++;
+    }
+  }
+  if(count === num){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
