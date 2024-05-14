75a8bdbfde8479c30baa8e9fa18532cfe124f494
raihanhasan <your.email@susickjoarder@gmail.com>
2024-05-13 20:43:32 -0400

    working prob 13

--- aa02-try-out-callbacks/problems/13-min-value-callback.js
+++ aa02-try-out-callbacks/problems/13-min-value-callback.js
@@ -12,7 +12,17 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 
 function minValueCallback(array, cb) {
     // Your code here 
+    //let result =0;
+    let min =array[0];
+    for(let i = 0; i < array.length; i++){
+      if(array[i]< min){
+        min = array[i]
+    }
+  }
+    return min
 }
+console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
